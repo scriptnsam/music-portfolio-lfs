5924c3db528dfb446d1f75747887de39bc193ec2
ScriptnSam <oluwafemisam40@gmail.com>
2024-09-25 04:29:46 +0100

    removed auto play and also added banner

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -3,7 +3,7 @@ const blogs = [
         id: 1,
         title: "Medley on Xylophone",
         description: "This is blog 1",
-        author: "Author 1",
+        author: "Ronke Aluko",
         date: "June 24, 2024",
         image: "https://res.cloudinary.com/dltvs77fa/image/upload/v1721273032/IMG-20240717-WA0072_dx0mro.jpg",
         category: "Medley",
@@ -13,5 +13,16 @@ const blogs = [
         As you watch the video, you'll be transported into a world of soul-stirring music and captivating rhythms. Ronke's talent shines brightly as she effortlessly plays the digital xylophone, infusing each note with passion and emotion. From the opening chords to the final crescendo, you'll find yourself completely immersed in the beauty of her performance.
 
         Join us on this musical journey as we celebrate the intersection of tradition and modernity in Ronke Aluko's Gospel Medley. Experience the magic of Lagos' thriving music culture and witness firsthand the incredible talent of this rising star. Get ready to be inspired and uplifted by the power of music!`
+    },
+    {
+        id: 2,
+        title: "New Live Worship Event Update",
+        description: "This is blog 2",
+        author: "Ronke Aluko",
+        date: "September 24, 2024",
+        image: "https://res.cloudinary.com/dltvs77fa/image/upload/v1727233507/live_worshy_nsmomg.jpg",
+        category: "Banner",
+        contentPreview: `Live Worhip Event...`,
+        content: `...`
     }
 ];
